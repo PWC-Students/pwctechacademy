dc9a03853ccef27a56ab49b3fc98e9d76689d7ca
Keanu John Lariosa <keanujohnlariosa@gmail.com>
2023-02-24 19:15:12 +0800

    added main.js

---
+++ main.js
@@ -0,0 +1,16 @@
+// type.js library
+
+var typed = new Typed(".type-animation", {
+  strings: [
+    "Teach and Inspire ^2000",
+    "Become an Instructor ^2000",
+    "Join PWCTechademy ^2000",
+  ],
+  typeSpeed: 30,
+  smartBackspace: true,
+  loop: true,
+  loopCount: Infinity,
+  backSpeed: 20,
+  cursorChar: "|",
+  shuffle: false,
+});
