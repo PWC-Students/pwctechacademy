8b2424e2e77435a5a55d43d5a26dbff75096890c
Keanu John Lariosa <keanujohnlariosa@gmail.com>
2023-03-02 17:01:55 +0800

    i dont know what to say

--- main.js
+++ main.js
@@ -33,6 +33,16 @@ const blogs = {
         read_min: "13 minutes read",
         popularity: "5.0",
       },
+      {
+        id: "article4",
+        title: "AHAHAHAHAH",
+        description: "This is sample data #1",
+        author: "Keanu John Lariosa",
+        author_profile: "https://i.ibb.co/FqRsSTg/profile-img1.jpg",
+        date_created: "2/23/2023",
+        read_min: "13 minutes read",
+        popularity: "2.0",
+      },
     ],
   },
   css: {
@@ -71,10 +81,271 @@ const blogs = {
       },
     ],
   },
+  javascript: {
+    id: "category3",
+    title: "Javascript",
+    articles: [
+      {
+        id: "article1",
+        title: "JavaScript - The Complete Guide 2023 (Beginner + Advanced)",
+        description: "This is sample data #1",
+        author: "Keanu John Lariosa",
+        author_profile: "https://i.ibb.co/FqRsSTg/profile-img1.jpg",
+        date_created: "3/2/2023",
+        read_min: "10 minutes read",
+        popularity: "4.5",
+      },
+      {
+        id: "article2",
+        title: "How to Use Map, Filter, and Reduce in JavaScript",
+        description: "This is sample data #1",
+        author: "Keanu John Lariosa",
+        author_profile: "https://i.ibb.co/FqRsSTg/profile-img1.jpg",
+        date_created: "2/28/2023",
+        read_min: "8 minutes read",
+        popularity: "5.0",
+      },
+      {
+        id: "article3",
+        title: "Timing Events in JavaScript: setTimeout and setInterval",
+        description: "This is sample data #1",
+        author: "Keanu John Lariosa",
+        author_profile: "https://i.ibb.co/FqRsSTg/profile-img1.jpg",
+        date_created: "2/28/2023",
+        read_min: "13 minutes read",
+        popularity: "3.0",
+      },
+    ],
+  },
+  reactjs: {
+    id: "category4",
+    title: "ReactJS",
+    articles: [
+      {
+        id: "article1",
+        title: "Learn React 18: The Complete React Developer Guide",
+        description: "This is sample data #1",
+        author: "Keanu John Lariosa",
+        author_profile: "https://i.ibb.co/FqRsSTg/profile-img1.jpg",
+        date_created: "3/2/2023",
+        read_min: "10 minutes read",
+        popularity: "4.5",
+      },
+      {
+        id: "article2",
+        title: "Learn React 18: Getting Started With State",
+        description: "This is sample data #1",
+        author: "Keanu John Lariosa",
+        author_profile: "https://i.ibb.co/FqRsSTg/profile-img1.jpg",
+        date_created: "2/28/2023",
+        read_min: "8 minutes read",
+        popularity: "5.0",
+      },
+      {
+        id: "article3",
+        title: "Learn React 18: Passing Data With Props",
+        description: "This is sample data #1",
+        author: "Keanu John Lariosa",
+        author_profile: "https://i.ibb.co/FqRsSTg/profile-img1.jpg",
+        date_created: "2/28/2023",
+        read_min: "13 minutes read",
+        popularity: "3.0",
+      },
+    ],
+  },
+  nextjs: {
+    id: "category5",
+    title: "ReactJS",
+    articles: [
+      {
+        id: "article1",
+        title: "A Complete Beginner's Guide to Next.js",
+        description: "This is sample data #1",
+        author: "Keanu John Lariosa",
+        author_profile: "https://i.ibb.co/FqRsSTg/profile-img1.jpg",
+        date_created: "3/2/2023",
+        read_min: "10 minutes read",
+        popularity: "4.5",
+      },
+      {
+        id: "article2",
+        title: "NextJs Tutorial: Getting Started with ReactJS Framework",
+        description: "This is sample data #1",
+        author: "Keanu John Lariosa",
+        author_profile: "https://i.ibb.co/FqRsSTg/profile-img1.jpg",
+        date_created: "2/28/2023",
+        read_min: "8 minutes read",
+        popularity: "5.0",
+      },
+      {
+        id: "article3",
+        title: "Next.js Quick Start Guide - Packt",
+        description: "This is sample data #1",
+        author: "Keanu John Lariosa",
+        author_profile: "https://i.ibb.co/FqRsSTg/profile-img1.jpg",
+        date_created: "2/28/2023",
+        read_min: "13 minutes read",
+        popularity: "3.0",
+      },
+    ],
+  },
+  laravel: {
+    id: "category6",
+    title: "ReactJS",
+    articles: [
+      {
+        id: "article1",
+        title: "How to Build a REST API With Laravel",
+        description: "This is sample data #1",
+        author: "Keanu John Lariosa",
+        author_profile: "https://i.ibb.co/FqRsSTg/profile-img1.jpg",
+        date_created: "3/2/2023",
+        read_min: "10 minutes read",
+        popularity: "4.5",
+      },
+      {
+        id: "article2",
+        title: "Building Web Applications From Scratch With Laravel",
+        description: "This is sample data #1",
+        author: "Keanu John Lariosa",
+        author_profile: "https://i.ibb.co/FqRsSTg/profile-img1.jpg",
+        date_created: "2/28/2023",
+        read_min: "8 minutes read",
+        popularity: "5.0",
+      },
+      {
+        id: "article3",
+        title: "Set Up an OAuth2 Server Using Passport in Laravel",
+        description: "This is sample data #1",
+        author: "Keanu John Lariosa",
+        author_profile: "https://i.ibb.co/FqRsSTg/profile-img1.jpg",
+        date_created: "2/28/2023",
+        read_min: "13 minutes read",
+        popularity: "3.0",
+      },
+    ],
+  },
+  python: {
+    id: "category7",
+    title: "Python",
+    articles: [
+      {
+        id: "article1",
+        title: "How to Begin With Test-Driven Development in Python",
+        description: "This is sample data #1",
+        author: "Keanu John Lariosa",
+        author_profile: "https://i.ibb.co/FqRsSTg/profile-img1.jpg",
+        date_created: "3/2/2023",
+        read_min: "10 minutes read",
+        popularity: "4.5",
+      },
+      {
+        id: "article2",
+        title: "Image Enhancement in Python",
+        description: "This is sample data #1",
+        author: "Keanu John Lariosa",
+        author_profile: "https://i.ibb.co/FqRsSTg/profile-img1.jpg",
+        date_created: "2/28/2023",
+        read_min: "8 minutes read",
+        popularity: "5.0",
+      },
+      {
+        id: "article3",
+        title: "Using the Requests Module in Python",
+        description: "This is sample data #1",
+        author: "Keanu John Lariosa",
+        author_profile: "https://i.ibb.co/FqRsSTg/profile-img1.jpg",
+        date_created: "2/28/2023",
+        read_min: "13 minutes read",
+        popularity: "3.0",
+      },
+    ],
+  },
+  java: {
+    id: "category8",
+    title: "Java",
+    articles: [
+      {
+        id: "article1",
+        title: "Java Tutorial for Beginners: Learn Core Java Programming",
+        description: "This is sample data #1",
+        author: "Keanu John Lariosa",
+        author_profile: "https://i.ibb.co/FqRsSTg/profile-img1.jpg",
+        date_created: "3/2/2023",
+        read_min: "10 minutes read",
+        popularity: "4.5",
+      },
+      {
+        id: "article2",
+        title: "Java Tutorial for Beginners - PWCTechademy",
+        description: "This is sample data #1",
+        author: "Keanu John Lariosa",
+        author_profile: "https://i.ibb.co/FqRsSTg/profile-img1.jpg",
+        date_created: "2/28/2023",
+        read_min: "8 minutes read",
+        popularity: "5.0",
+      },
+      {
+        id: "article3",
+        title: "Java Programming : A Complete Guide",
+        description: "This is sample data #1",
+        author: "Keanu John Lariosa",
+        author_profile: "https://i.ibb.co/FqRsSTg/profile-img1.jpg",
+        date_created: "2/28/2023",
+        read_min: "13 minutes read",
+        popularity: "3.0",
+      },
+    ],
+  },
+  php: {
+    id: "category9",
+    title: "Java",
+    articles: [
+      {
+        id: "article1",
+        title: "How to Upload a File in PHP (With an Example)",
+        description: "This is sample data #1",
+        author: "Keanu John Lariosa",
+        author_profile: "https://i.ibb.co/FqRsSTg/profile-img1.jpg",
+        date_created: "3/2/2023",
+        read_min: "10 minutes read",
+        popularity: "4.5",
+      },
+      {
+        id: "article2",
+        title: "How to Use Sessions and Session Variables in PHP",
+        description: "This is sample data #1",
+        author: "Keanu John Lariosa",
+        author_profile: "https://i.ibb.co/FqRsSTg/profile-img1.jpg",
+        date_created: "2/28/2023",
+        read_min: "8 minutes read",
+        popularity: "5.0",
+      },
+      {
+        id: "article3",
+        title: "How to Use PHP in HTML",
+        description: "This is sample data #1",
+        author: "Keanu John Lariosa",
+        author_profile: "https://i.ibb.co/FqRsSTg/profile-img1.jpg",
+        date_created: "2/28/2023",
+        read_min: "13 minutes read",
+        popularity: "3.0",
+      },
+    ],
+  },
 };
 
 const popular = document.querySelector(".glide__slides__popular");
 const recent = document.querySelector(".glide__slides__recent");
+const html = document.querySelector(".glide__slides__html");
+const css = document.querySelector(".glide__slides__css");
+const javascript = document.querySelector(".glide__slides__javascript");
+const reactjs = document.querySelector(".glide__slides__reactjs");
+const nextjs = document.querySelector(".glide__slides__nextjs");
+const laravel = document.querySelector(".glide__slides__laravel");
+const python = document.querySelector(".glide__slides__python");
+const java = document.querySelector(".glide__slides__java");
+const php = document.querySelector(".glide__slides__php");
 
 // converts object into an array
 const articleArray = [];
@@ -84,6 +355,7 @@ Object.values(blogs).forEach((key) => {
 
 function articleMap(data, sortType) {
   data.innerHTML = sortType
+    .slice(0, 9)
     .map(
       (article) =>
         `
@@ -103,16 +375,31 @@ function articleMap(data, sortType) {
     .join("");
 }
 
+const filteredData = articleArray.filter(
+  (item) => item.popularity >= 4.0 && item.popularity <= 5.0
+);
+
 articleMap(
   popular,
-  articleArray.sort((a, b) => b.popularity - a.popularity)
+  filteredData.sort((a, b) => b.popularity - a.popularity)
 );
+
 articleMap(
   recent,
   articleArray.sort(
     (a, b) => new Date(b.date_created) - new Date(a.date_created)
   )
 );
+
+articleMap(html, blogs.html.articles);
+articleMap(css, blogs.css.articles);
+articleMap(javascript, blogs.javascript.articles);
+articleMap(reactjs, blogs.reactjs.articles);
+articleMap(nextjs, blogs.nextjs.articles);
+articleMap(laravel, blogs.laravel.articles);
+articleMap(python, blogs.python.articles);
+articleMap(java, blogs.java.articles);
+articleMap(php, blogs.php.articles);
 
 //drag slide
 function dragSlide(data) {
@@ -145,9 +432,17 @@ function dragSlide(data) {
 
 dragSlide(popular);
 dragSlide(recent);
+dragSlide(html);
+dragSlide(css);
+dragSlide(javascript);
+dragSlide(reactjs);
+dragSlide(nextjs);
+dragSlide(laravel);
+dragSlide(python);
+dragSlide(java);
+dragSlide(php);
 
 function slider(data) {
-  console.log(data);
   const slide = document.querySelector(".glide__slide");
   const nextButton = document.querySelector(".glide__next");
   const slideWidth = slide.clientWidth;
