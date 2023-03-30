eaca3c70299ad13abbada2f1a62112bc754788d7
Keanu John Lariosa <keanujohnlariosa@gmail.com>
2023-03-30 19:51:52 +0800

    added header, navbar and footer

---
+++ scripts/main.js
@@ -0,0 +1,17 @@
+let navToggle = document.querySelector('.nav-toggle')
+let navToggle2 = document.querySelector('.nav-toggle2')
+let bars = document.querySelectorAll('.bar')
+let sidebar = document.querySelector('.sidebar')
+
+function toggleHamburger(e) {
+  bars.forEach((bar) => bar.classList.toggle('x'))
+  sidebar.classList.add('active')
+}
+
+function unToggleHamburger(e) {
+  bars.forEach((bar) => bar.classList.toggle('x'))
+  sidebar.classList.remove('active')
+}
+
+navToggle.addEventListener('click', toggleHamburger)
+navToggle2.addEventListener('click', unToggleHamburger)
